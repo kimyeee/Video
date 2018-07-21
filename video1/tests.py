94514c43814ee964715dbaabe7eb7a594866b54b
kimyeee <kimyeee@163.com>
2018-07-21 21:31:54 +0800

    commit

--- video1/tests.py
+++
@@ -1,7 +0,0 @@
-from django.test import TestCase
-
-# Create your tests here.
-import requests
-
-res = requests.get('https://code.jquery.com/jquery-3.3.1.js')
-open('jquery-3.3.1.js','wb').write(res.content)
