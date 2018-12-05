07931d9579ec57090fd23bfdbc4e04fa69b6cd2b
kimyeee <kimyeee@163.com>
2018-12-05 11:56:31 +0800

    commit

--- celery_/tasks.py
+++ celery_/tasks.py
@@ -1,7 +1,17 @@
+import time
 from celery import Celery
 
-app = Celery('tasks', broker='amqp://guest@localhost//')
+app = Celery('tasks', backend='amqp://guest@localhost', broker='amqp://guest@localhost//')
+
 
 @app.task
 def add(x, y):
+    time.sleep(3)
+    # print(x, y)
+    # open(r'D:\project\Video\celery_\test.txt', 'w').write('test')
     return x + y
+
+
+if __name__ == '__main__':
+    result = add.delay(30, 42)
+    print(result)
