560215c121853fa91e82e53232fd89bc5a9c091f
kimyeee <kimyeee@163.com>
2018-12-19 18:19:32 +0800

    commit

--- lagou/cloud.py
+++ lagou/cloud.py
@@ -30,7 +30,7 @@ print('加载图片成功！')
 wc = WordCloud(
     background_color='white',  # 设置背景颜色
     mask=backgroud_Image,  # 设置背景图片
-    font_path='C:\Windows\Fonts\YuGothR.ttc',  # 若是有中文的话，这句代码必须添加，不然会出现方框，不出现汉字
+    font_path='C:\Windows\Fonts\YuGothM.ttc',  # 若是有中文的话，这句代码必须添加，不然会出现方框，不出现汉字
     max_words=2000,  # 设置最大现实的字数
     stopwords=STOPWORDS,  # 设置停用词
     max_font_size=150,  # 设置字体最大值
